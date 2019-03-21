dcd03b8667e8cc31ae424d191626db2db0f70719
bluerat1976 <osoblivaia76@mail.ru>
2019-03-21 11:40:47 +0200

    The strange result

---
+++ index.js
@@ -0,0 +1,34 @@
+debugger;
+
+/*
+var a = prompt( 'input a' );
+var b = prompt( 'input b' );
+var c = prompt( 'input c' );
+
+a = parseInt( a );
+b = parseInt( b );
+c = parseInt( c );
+*/
+
+a = 3;
+b = 9;
+c = 2;
+
+function getDis ( a, b, c ) { 
+    return b*b - 4*a*c;
+}
+alert( getDis() );
+
+/*
+if( result < 0 ) {
+    alert('There are not datas');
+
+} else if(results === 0) {
+   var x = - b / (2 *a);
+    
+    } else {
+        x = (- b + Math.sqrt(result)) / (2 *a);
+    }  
+
+alert ( x );
+*/
